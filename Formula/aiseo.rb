9030db09f764b4e44aebea4ed9ad8dda0444e1e3
Boris Djordjevic <dboris@gmail.com>
2026-05-24 05:49:46 +0100

    aiseo: bump to 0.6.0 (design_slop + metatext + copy_precision)

--- Formula/aiseo.rb
+++ Formula/aiseo.rb
@@ -1,8 +1,8 @@
 class Aiseo < Formula
   desc "Agent-first SEO / GEO / AEO auditor"
   homepage "https://github.com/paperfoot/aiseo"
-  url "https://github.com/paperfoot/aiseo/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "81050c29f26bd65b354ee97653ec0a3d9d8c06123220792437d14f8ff98c5111"
+  url "https://github.com/paperfoot/aiseo/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "871dbae2c5b5e8334a657f5496d5609f5aee377ae24dfea95a71db703b77c14f"
   license "MIT"
   head "https://github.com/paperfoot/aiseo.git", branch: "main"
 
