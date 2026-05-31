bb5e13df328d62e757bdb4e1a6415c3caa2b6602
Boris Djordjevic <dboris@gmail.com>
2026-05-31 18:51:19 +0100

    search-cli 0.6.2 (working self-update)

--- Formula/search-cli.rb
+++ Formula/search-cli.rb
@@ -1,16 +1,16 @@
 class SearchCli < Formula
   desc "Agent-friendly multi-provider search CLI with email verification (12 providers, 13 modes)"
   homepage "https://github.com/paperfoot/search-cli"
-  version "0.6.1"
+  version "0.6.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/paperfoot/search-cli/releases/download/v0.6.1/search-aarch64-apple-darwin.tar.gz"
-      sha256 "a12b5d0deca96e68d88e90bd2085122fad771417e0f3ed23cb4a61b94471e361"
+      url "https://github.com/paperfoot/search-cli/releases/download/v0.6.2/search-aarch64-apple-darwin.tar.gz"
+      sha256 "f52ca13d9c26f6fce7ec6839a1dc3904f05c8497b7891e47fd50948ad254c831"
     else
-      url "https://github.com/paperfoot/search-cli/releases/download/v0.6.1/search-x86_64-apple-darwin.tar.gz"
-      sha256 "a5f69d9595b536e491a6cbe70873a768b768f5c36c0147b78fba5243356cd6ac"
+      url "https://github.com/paperfoot/search-cli/releases/download/v0.6.2/search-x86_64-apple-darwin.tar.gz"
+      sha256 "25885fb9276b54f480eb6099470a2f6ebcbf18952a8eb75928ae390ba3376bdf"
     end
   end
 
@@ -19,7 +19,7 @@ class SearchCli < Formula
   end
 
   test do
-    assert_match "search 0.6.1", shell_output("#{bin}/search --version")
+    assert_match "search 0.6.2", shell_output("#{bin}/search --version")
     assert_match "verify", shell_output("#{bin}/search --help")
   end
 end
